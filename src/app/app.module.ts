a5328e84f1c545d06696ce7b7f6db0f28915cb74
Sujitha-ss501 <snowashii2002@gmail.com>
2023-11-24 10:00:04 +0530

    loader changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { CoreModule } from './core/core.module';
+import { NgxUiLoaderModule, NgxUiLoaderConfig, PB_DIRECTION, SPINNER } from 'ngx-ui-loader';
 
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  text: "Loading...",
+  textColor: "#FFFFFF",
+  textPosition: "center-center",
+  pbColor:"#2e0808",
+  bgsColor:"#2e0808",
+  fgsColor:"#2e0808",
+  fgsType: SPINNER.threeStrings,
+  fgsSize: 100,
+  pbDirection: PB_DIRECTION.leftToRight,
+  pbThickness: 5
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +29,8 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    CoreModule
+    CoreModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
